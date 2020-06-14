2be53ce8826623b46db13bf518917f606d80fc92
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-14 12:25:17 +0700

    Initial file & commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIS Desa Duren</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="css/bootstrap.css"">
+    <!-- Custom CSS -->
+    <link rel=" stylesheet" href="css/style.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="css/all.css">
+
+</head>
+
+<body>
+    <div class="jumbotron">
+        <h1>File Awal</h1>
+    </div>
+</body>
+
+</html>
